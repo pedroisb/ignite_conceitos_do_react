de159e593c0bc55110eb99a96b135652aea3d5dc
Pedro Brito <pedroisb28@gmail.com>
2022-04-12 17:46:39 -0300

    challenge complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,56 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    // created object newTask to store values of new task
+    const newTask: Task = {
+      id: Math.floor(Math.random()*101),
+      title: `${newTaskTitle}`,
+      isComplete: false,
+    }
+
+    // trim() will remove whitespaces, so as to make it easier to check for empty strings
+    if(newTaskTitle.trim() !== ''){ 
+      // since tasks is an array, we'll need to use spread operator in order to update it's value with the new element
+      // the order here matters
+      setTasks([
+        ...tasks,
+        newTask
+      ])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    // need to use any for type since undefined is a potential return type in find()
+    const foundTask: any = tasks.find(el => el.id === id);
+
+    // found task index in order to swap it later on
+    const foundTaskIndex = tasks.indexOf(foundTask);
+
+    // updated foundTask isComplete prop
+    foundTask['isComplete'] = true;
+
+    // created updatedTaskList and set it to tasks
+    const updatedTaskList = tasks;
+
+    // swapping old task with updated one
+    updatedTaskList[foundTaskIndex] = foundTask;
+
+    // usage of spread operator is mandatory here, otherwise React won't trigger re-render, it'll not compare the values of the old state with the updated one
+    // in depth: updatedTaskList isn't a new array, it's the same array stored in tasks, but mutated
+    // handleRemoveTask works without spread because filteredTaskList is indeed a NEW array; remember that filter() returns a new array, it doesn't mutate the original one
+    setTasks([...updatedTaskList]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    // filter() returns a new array list, which is ideal to be used with a setState(). I don't want to alter the original array, but set a new one, in order to trigger a re-render
+    const filteredTaskList: Task[] = tasks.filter(el => el.id !== id);
+
+    setTasks(filteredTaskList);
   }
 
   return (
